b1a978bf251720faac17ac689295f1b80cb35751
krishnadas <krishnadas>
2016-11-01 18:16:40 +0530

    initialized the repo.

---
+++ site/meadowlark.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var app = express();
+
+//Include the required module
+var fortune = require('./lib/fortune.js');
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//Set server port
+app.set('port', process.env.PORT || 3000);
+
+//Declare the static path for static files
+app.use(express.static(__dirname + '/public'));
+
+//For running tests
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+	req.query.test === '1';
+next();
+});
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+app.get('/about', function(req, res){
+	res.render('about', { fortune: fortune.getFortune() });
+});
+//404 catch-all handler (middleware)
+app.use(function(req, res) {
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log( 'Express started on http://localhost:' +
+		app.get('port') + '; press Ctrl-C to terminate.' );
+});
